801527ebd5e2df0d7ea5b05cf25ac2891c0c4f16
Laura Mickes <laura.mickes@rhul.ac.uk>
2020-12-07 12:46:39 +0000

    Add standard setuptools

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyWitness',
+    version='0.1',
+    packages=find_packages(exclude=["docs"]),
+    # Not sure how strict these need to be...
+    install_requires=["matplotlib",
+                      "numpy",
+                      "scipy",
+                      "pandas"]
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    python_requires=">=3.7.*",
+
+    author='Laura Mickes',
+    author_email='laura.mickes@rhul.ac.uk',
+    description=("Python utilities for eyewitness research"),
+    license="GPL3",
+    url='https'
+)
