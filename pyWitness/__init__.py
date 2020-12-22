bdd948e8b568f600581e1496bf387e153c4b20ab
Laura Mickes <laura.mickes@rhul.ac.uk>
2020-12-22 00:00:46 +0000

    Indep observation model

--- pyWitness/__init__.py
+++ pyWitness/__init__.py
@@ -1,4 +1,5 @@
 from .DataRaw import DataRaw, dataMapSdtlu, dataMapPyWitness
 from .DataProcessed import DataProcessed 
+from .ModelFits import ModelFitIndependentObservation
 
 print("pyWitness v0.1 : Laura Mickes")
