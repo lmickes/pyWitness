bf3625df2cb9450c6d1e44de6bf9df9d993bbfa0
Laura Mickes <laura.mickes@rhul.ac.uk>
2021-02-27 16:05:43 +0000

    Wilson, Seale-Carlisle, Mickes paper

--- pyWitness/DataTranslator.py
+++ pyWitness/DataTranslator.py
@@ -1,5 +1,6 @@
 import pandas as _pandas
 import numpy as _np
+import copy as _copy
 from .DataRaw import DataRaw
 
 def relabelConfidenceForShowups(df) :
@@ -7,6 +8,66 @@ def relabelConfidenceForShowups(df) :
     df["confidence"][_np.logical_and(df["lineupSize"] == 1, df["responseType"] == "rejectId")] = -df["confidence"] - conf_min
 
     df["responseType"][_np.logical_and(_np.logical_and(df["responseType"] == "fillerId", df["targetLineup"] == "targetAbsent"), df["lineupSize"]==1)] = "suspectId"
+
+def published_Wilson_2018_Experiment12(fileName = "Wilson_SealeCarlisle_Mickes2017.xlsx", excelSheet = "Exp1_2") :
+
+    # load spreadsheet
+    data = _pandas.read_excel(fileName, excelSheet, engine='openpyxl')
+
+    # get important data
+    participantId    = data['ID #']
+    targetLineup     = data['Target Absent or Present']
+    lineupSize       = _copy.copy(data['Present or Absent Response'])   # copy column
+    accuracy         = data['Accuracy']
+    response         = data['Present or Absent Response']
+    responseType     = _copy.copy(data['Present or Absent Response'])   # copy column
+    confidence       = data['Confidence']
+
+    # translate data
+    targetLineup.replace({"Target-absent":"targetAbsent", "Target-present":"targetPresent"}, inplace=True)
+    lineupSize.loc[:] = 6
+
+    taFillerId       = _np.logical_and(_np.logical_and(targetLineup == "targetAbsent",  accuracy == 0),response == "Present")
+    taRejectId       = _np.logical_and(_np.logical_and(targetLineup == "targetAbsent",  accuracy == 1),response == "Absent")
+    tpFillerId       = _np.logical_and(_np.logical_and(targetLineup == "targetPresent", accuracy == 0),response == "Present")
+    tpSuspectId      = _np.logical_and(_np.logical_and(targetLineup == "targetPresent", accuracy == 1),response == "Present")
+    tpRejectId       = _np.logical_and(_np.logical_and(targetLineup == "targetPresent", accuracy == 0),response == "Absent")
+
+    responseType.loc[taFillerId]  = "fillerId"
+    responseType.loc[taRejectId]  = "rejectId"
+    responseType.loc[tpFillerId]  = "fillerId"
+    responseType.loc[tpSuspectId] = "suspectId"
+    responseType.loc[tpRejectId]  = "rejectId"
+
+    # get other data
+    experiment       = data['Exp']
+    age              = data['Age']
+    gender           = data['Gender']
+    group            = data['Group']
+    description      = data['Description']
+    previouslyViewed = data['Previously Viewed Video']
+
+    dataNew = _pandas.DataFrame()
+    dataNew = dataNew.assign(participantId    = participantId)
+    dataNew = dataNew.assign(targetLineup     = targetLineup)
+    dataNew = dataNew.assign(lineupSize       = lineupSize)
+    dataNew = dataNew.assign(responseType     = responseType)
+    dataNew = dataNew.assign(confidence       = confidence)
+    dataNew = dataNew.assign(experiment       = experiment)
+    dataNew = dataNew.assign(age              = age)
+    dataNew = dataNew.assign(gender           = gender)
+    dataNew = dataNew.assign(group            = group)
+    dataNew = dataNew.assign(description      = description)
+    dataNew = dataNew.assign(previouslyViewed = previouslyViewed)
+
+    dr = DataRaw('')
+    dr.data = dataNew
+
+    return dr
+
+def published_Wilson_2018_Experiment34(fileName = "Wilson_SealeCarlisle_Mickes2017.xlsx", excelSheet = "Exp3") :
+    pass
+
 
 def published_Akan_2020_Experiment1(fileName = "experiment1", excelSheet = 'E1') :
     data = _pandas.read_excel(fileName, excelSheet, engine='openpyxl')
