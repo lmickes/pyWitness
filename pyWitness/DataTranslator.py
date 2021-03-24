21a88697b92d58cafdd06eb973aced4544bc43c2
Laura Mickes <laura.mickes@bristol.ac.uk>
2021-03-24 15:11:23 +0000

    More testing and data translators

--- pyWitness/DataTranslator.py
+++ pyWitness/DataTranslator.py
@@ -438,8 +438,125 @@ def published_SealeCarlisle_Wetmore_Flowe_Mickes_2019_E5(fileName = "", excelShe
 
     return dr       
 
-
 #########################################################################################################
+
+def published_Colloff_SealeCarlisle_Karoğlu_etal2020_E1(fileName = "") :
+
+    if fileName == "" :
+        fileName = _dir+"/../data/published/2020_Colloff_SealeCarlisle_Karoğlu_etal/Exp1_Data.csv"
+
+    # load spreadsheet
+    data = openCsvFile(fileName)
+
+    # get important data
+    targetLineup     = data['targetPresentRaw']
+    lineupSize       = _copy.copy(data['videoRaw'])   # copy column
+    accuracy         = data['accuracy']
+    response         = data['participantTargetPresent']
+    responseType     = _copy.copy(data['videoRaw'])   # copy column
+    confidence       = data['confidence']
+
+    # translate data
+    targetLineup.replace({"Target Absent":"targetAbsent", "Target Present":"targetPresent"}, inplace=True)
+    lineupSize.loc[:] = 9
+
+    taFillerId       = _np.logical_and(_np.logical_and(targetLineup == "targetAbsent",  accuracy == 0),response == 1)
+    taRejectId       = _np.logical_and(_np.logical_and(targetLineup == "targetAbsent",  accuracy == 1),response == 0)
+    tpFillerId       = _np.logical_and(_np.logical_and(targetLineup == "targetPresent", accuracy == 0),response == 1)
+    tpSuspectId      = _np.logical_and(_np.logical_and(targetLineup == "targetPresent", accuracy == 1),response == 1)
+    tpRejectId       = _np.logical_and(_np.logical_and(targetLineup == "targetPresent", accuracy == 0),response == 0)
+
+    responseType.loc[taFillerId]  = "fillerId"
+    responseType.loc[taRejectId]  = "rejectId"
+    responseType.loc[tpFillerId]  = "fillerId"
+    responseType.loc[tpSuspectId] = "suspectId"
+    responseType.loc[tpRejectId]  = "rejectId"
+
+    # get other data
+    condition        = data['condition']
+    age              = data['age']
+    gender           = data['sex']
+    ethnicity        = data['ethnicity']
+    keep             = data['keep']
+    
+    dataNew = _pandas.DataFrame()
+    dataNew = dataNew.assign(targetLineup     = targetLineup)
+    dataNew = dataNew.assign(lineupSize       = lineupSize)
+    dataNew = dataNew.assign(responseType     = responseType)
+    dataNew = dataNew.assign(confidence       = confidence)
+
+    dataNew = dataNew.assign(condition        = condition)
+    dataNew = dataNew.assign(age              = age)
+    dataNew = dataNew.assign(gender           = gender)
+    dataNew = dataNew.assign(ethnicity        = ethnicity)
+    dataNew = dataNew.assign(keep             = keep)
+
+    dr = DataRaw('')
+    dr.data = dataNew
+    dr.checkData()
+
+    return dr
+
+def published_Colloff_SealeCarlisle_Karoğlu_etal2020_E2(fileName = "") :
+
+    if fileName == "" :
+        fileName = _dir+"/../data/published/2020_Colloff_SealeCarlisle_Karoğlu_etal/Exp2_Data.csv"
+
+    # load spreadsheet
+    data = openCsvFile(fileName)
+
+    # get important data
+    participantId    = data['ParticipantId']
+    targetLineup     = data['TargetPresent']
+    lineupSize       = _copy.copy(data['Age'])   # copy column
+    accuracy         = data['Accuracy']
+    response         = data['ParticipantTargetPresent']
+    responseType     = _copy.copy(data['Age'])   # copy column
+    confidence       = data['Confidence']
+
+    # translate data
+    targetLineup.replace({0:"targetAbsent", 1:"targetPresent"}, inplace=True)
+    lineupSize.loc[:] = 6
+
+    taFillerId       = _np.logical_and(_np.logical_and(targetLineup == "targetAbsent",  accuracy == 0),response == "yes")
+    taRejectId       = _np.logical_and(_np.logical_and(targetLineup == "targetAbsent",  accuracy == 1),response == "no")
+    tpFillerId       = _np.logical_and(_np.logical_and(targetLineup == "targetPresent", accuracy == 0),response == "yes")
+    tpSuspectId      = _np.logical_and(_np.logical_and(targetLineup == "targetPresent", accuracy == 1),response == "yes")
+    tpRejectId       = _np.logical_and(_np.logical_and(targetLineup == "targetPresent", accuracy == 0),response == "no")
+
+    responseType.loc[taFillerId]  = "fillerId"
+    responseType.loc[taRejectId]  = "rejectId"
+    responseType.loc[tpFillerId]  = "fillerId"
+    responseType.loc[tpSuspectId] = "suspectId"
+    responseType.loc[tpRejectId]  = "rejectId"
+
+    # get other data
+    condition        = data['EncodingConditionRaw']
+    age              = data['Age']
+    gender           = data['Sex']
+    ethnicity        = data['Ethnicity']
+    
+    dataNew = _pandas.DataFrame()
+    dataNew = dataNew.assign(participantId    = participantId)
+    dataNew = dataNew.assign(targetLineup     = targetLineup)
+    dataNew = dataNew.assign(lineupSize       = lineupSize)
+    dataNew = dataNew.assign(accuracy         = accuracy)
+    dataNew = dataNew.assign(response         = response)
+    dataNew = dataNew.assign(responseType     = responseType)
+    dataNew = dataNew.assign(confidence       = confidence)
+
+    dataNew = dataNew.assign(condition        = condition)
+    dataNew = dataNew.assign(age              = age)
+    dataNew = dataNew.assign(gender           = gender)
+    dataNew = dataNew.assign(ethnicity        = ethnicity)
+
+    dr = DataRaw('')
+    dr.data = dataNew
+    dr.checkData()
+
+    return dr
+
+#########################################################################################################   
 def published_Morgan_2019(fileName = "", excelSheet = "Raw Data") :
 
     if fileName == "" :
