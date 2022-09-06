2ef256b3812c842913ec26375523b33646b5379e
sboogert <stewart.boogert@rhul.ac.uk>
2022-09-06 14:02:02 +0100

    Prepare for release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     python_requires=">=3.7.*",
 
     author='Laura Mickes',
-    author_email='laura.mickes@rhul.ac.uk',
+    author_email='laura.mickes@bristol.ac.uk',
     description=("Python utilities for eyewitness research"),
     license="GPL3",
     url='https://github.com/lmickes/pyWitness'
