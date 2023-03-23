16c078291e8380ea7b831719a0d85441f4b8eb3d
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2023-03-23 09:43:44 +0000

    Fix proportion correct calcualtion for showups

--- pyWitness/DataProcessed.py
+++ pyWitness/DataProcessed.py
@@ -271,14 +271,15 @@ class DataProcessed :
         if self.lineupSize != 1 :                                                                           # SHOWUP
             cid = self.data_pivot.loc['targetPresent','suspectId']
         else :
-            cid = self.data_pivot.loc['targetPresent', 'suspectId'] + self.data_pivot.loc['targetPresent', 'rejectId']
-
+            #cid = self.data_pivot.loc['targetPresent', 'suspectId'] + self.data_pivot.loc['targetPresent', 'rejectId']
+            cid = self.data_pivot.loc['targetPresent', 'suspectId'] + self.data_pivot.loc['targetAbsent', 'rejectId']
         try :
             if self.lineupSize != 1 :                                                                       # SHOWUP
                 fid = self.data_pivot.loc['targetAbsent','suspectId']
             else :
-                fid = self.data_pivot.loc['targetAbsent', 'suspectId'] + self.data_pivot.loc['targetAbsent', 'rejectId']
-        except KeyError :            
+                # fid = self.data_pivot.loc['targetAbsent', 'suspectId'] + self.data_pivot.loc['targetAbsent', 'rejectId']
+                fid = self.data_pivot.loc['targetPresent', 'rejectId'] + self.data_pivot.loc['targetAbsent', 'suspectId']
+        except KeyError :
             fid = self.data_pivot.loc['targetAbsent','fillerId']/self.lineupSize
         
         cac = cid/(cid+fid)
