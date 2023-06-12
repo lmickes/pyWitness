bf218f09143fad2a683029a7ee05e69f59166f95
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2023-06-12 22:13:01 +0100

    Fix CAC for designated innocent

--- pyWitness/DataProcessed.py
+++ pyWitness/DataProcessed.py
@@ -269,18 +269,18 @@ class DataProcessed :
         '''
 
         if self.lineupSize != 1 :                                                                           # SHOWUP
-            cid = self.data_pivot.loc['targetPresent','suspectId']
+            cid = self.data_rates.loc['targetPresent','suspectId']
         else :
             #cid = self.data_pivot.loc['targetPresent', 'suspectId'] + self.data_pivot.loc['targetPresent', 'rejectId']
-            cid = self.data_pivot.loc['targetPresent', 'suspectId'] + self.data_pivot.loc['targetAbsent', 'rejectId']
+            cid = self.data_rates.loc['targetPresent', 'suspectId'] + self.data_rates.loc['targetAbsent', 'rejectId']
         try :
             if self.lineupSize != 1 :                                                                       # SHOWUP
-                fid = self.data_pivot.loc['targetAbsent','suspectId']
+                fid = self.data_rates.loc['targetAbsent','suspectId']
             else :
                 # fid = self.data_pivot.loc['targetAbsent', 'suspectId'] + self.data_pivot.loc['targetAbsent', 'rejectId']
-                fid = self.data_pivot.loc['targetPresent', 'rejectId'] + self.data_pivot.loc['targetAbsent', 'suspectId']
+                fid = self.data_rates.loc['targetPresent', 'rejectId'] + self.data_rates.loc['targetAbsent', 'suspectId']
         except KeyError :
-            fid = self.data_pivot.loc['targetAbsent','fillerId']/self.lineupSize
+            fid = self.data_rates.loc['targetAbsent','fillerId']/self.lineupSize
         
         cac = cid/(cid+fid)
         cac.name = ("cac","central")
