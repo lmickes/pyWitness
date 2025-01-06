154f0c76624ad91010a37a91fc755eba018313b2
Laura Mickes <laura.mickes@bristol.ac.uk>
2025-01-06 19:23:08 +0000

    add tests

--- test/test_05_fitting.py
+++ test/test_05_fitting.py
@@ -60,3 +60,40 @@ def test_05_fitting_test1_csv_set_parameters_set_equal_var():
     mf.setEqualVariance()
     mf.setParameterEstimates()
     mf.printParameters()
+
+def test_05_fitting_test1_csv_plot_fit_roc():
+    import pyWitness
+    dr = pyWitness.DataRaw("../data/tutorial/test1.csv")
+    dr.collapseContinuousData(column="confidence", bins=[-1, 60, 80, 100], labels=None)
+    dp = dr.process()
+    dp.calculateConfidenceBootstrap(nBootstraps=200)
+    mf = pyWitness.ModelFitIndependentObservation(dp)
+    mf.setEqualVariance()
+    mf.fit()
+    dp.plotROC(label="Data")
+    mf.plotROC(label="Indep. obs. fit")
+
+def test_05_fitting_test1_csv_plot_fit():
+    import pyWitness
+    dr = pyWitness.DataRaw("../data/tutorial/test1.csv")
+    dr.collapseContinuousData(column="confidence", bins=[-1, 60, 80, 100], labels=None)
+    dp = dr.process()
+    dp.calculateConfidenceBootstrap(nBootstraps=200)
+    mf = pyWitness.ModelFitIndependentObservation(dp)
+    mf.setEqualVariance()
+    mf.fit()
+    dp.plotROC(label="Data")
+    mf.plotROC(label="Indep. obs. fit")
+    mf.plotFit()
+
+def test_05_fitting_test1_csv_plot_fit_cac():
+    import pyWitness
+    dr = pyWitness.DataRaw("../data/tutorial/test1.csv")
+    dr.collapseContinuousData(column="confidence", bins=[-1, 60, 80, 100], labels=None)
+    dp = dr.process()
+    dp.calculateConfidenceBootstrap(nBootstraps=200)
+    mf = pyWitness.ModelFitIndependentObservation(dp)
+    mf.setEqualVariance()
+    mf.fit()
+    dp.plotCAC(label="Data")
+    mf.plotCAC(label="Indep. obs. fit")
