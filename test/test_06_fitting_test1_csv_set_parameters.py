5835c4a1f3cadcc8dc7a52623d22f1bbbec3e1f1
Laura Mickes <laura.mickes@bristol.ac.uk>
2025-01-06 19:09:41 +0000

    add tests

--- test/test_06_fitting_test1_csv_set_parameters.py
+++ test/test_06_fitting_test1_csv_set_parameters.py
@@ -14,3 +14,11 @@ def test_06_fitting_test1_csv_set_parameters_print_parameters():
     mf = pyWitness.ModelFitIndependentObservation(dp)
     mf.printParameters()
 
+def test_06_fitting_test1_csv_set_parameters_set_equal_var():
+    import pyWitness
+    dr = pyWitness.DataRaw("../data/tutorial/test1.csv")
+    dr.collapseContinuousData(column="confidence", bins=[-1, 60, 80, 100], labels=[1, 2, 3])
+    dp = dr.process()
+    mf.setEqualVariance()
+    mf.setParameterEstimates()
+    mf.printParameters()
