27ec0d91113a52338069c06f6a7d41fa55063e76
Laura Mickes <laura.mickes@bristol.ac.uk>
2025-01-07 18:37:16 +0000

    checking values

--- test/test_01_loading.py
+++ test/test_01_loading.py
@@ -3,9 +3,10 @@ import pytest
 def test_01_loading_test1_csv():
     import pyWitness
     dr = pyWitness.DataRaw("../data/tutorial/test1.csv")
-    assert dr.numberLineups == 890
-    assert dr.numberTALineups == 443
-    assert dr.numberTPLineups == 447
+    dp = dr.process()
+    assert dp.numberLineups == 890
+    assert dp.numberTALineups == 443
+    assert dp.numberTPLineups == 447
 
 def test_01_loading_test1_csv_check_data():
     import pyWitness
